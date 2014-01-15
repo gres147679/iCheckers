1443c5ffcd26569bbc31e10e254c5a8e96006e4e
Wilmer Bandres <wilmer0593@hotmail.com>
2014-01-15 01:38:13 -0430

    imprime la verga esta

--- proyecto.pl
+++ proyecto.pl
@@ -1,10 +1,13 @@
 
-jugar :- assert(tablero([[0,0,2],[0,2,2],[0,4,2],[0,6,2],
-		 [1,1,2],[1,3,2],[1,5,2],[1,7,2],
-		 [2,0,2],[2,2,2],[2,4,2],[2,6,2],		 
-		 [7,1,1],[7,3,1],[7,5,1],[7,7,1],
-		 [6,0,1],[6,2,1],[6,4,1],[6,6,1],
-		 [5,1,1],[5,3,1],[5,5,1],[5,7,1]
+%Comando que inicia el juego, asi como el tablero del mismo
+jugar :- assert(tablero(
+		 [
+		 [1,2,1],[1,4,1],[1,6,1],[1,8,1],
+		 [2,1,1],[2,3,1],[2,5,1],[2,7,1],
+		 [3,2,1],[3,4,1],[3,6,1],[3,8,1],		 
+		 [8,1,2],[8,3,2],[8,5,2],[8,7,2],
+		 [7,2,2],[7,4,2],[7,6,2],[7,8,2],
+		 [6,1,2],[6,3,2],[6,5,2],[6,7,2]
 		 ])).
 	
 % Verifica si existe una ficha en el tablero	
@@ -12,28 +15,76 @@ existeFicha(X,Y,Z):-
 	tablero(T), 
 	member([X,Y,Z],T).
 
+impCar(X):-
+	X is 1,
+	write('|< | ').
+impCar(X):-
+	X is 2,
+	write('|> | ').
+impCar(X):-
+	X is 3,
+	write('|<<| ').
+impCar(X):-
+	X is 4,
+	write('|>>| ').
 
 
+%verifica para imprimir un caracter del tablero
+imprimirCar(F,C,X):-
+	member([F,C,Z],X),
+	impCar(Z),
+	!.
+imprimirCar(F,C,X):-
+	write('|  | ').
+
+%imprime una fila
+imprimirFila(F,X):-
+	write(F),
+	write(' '),
+	imprimirCar(F,1,X),
+	imprimirCar(F,2,X),
+	imprimirCar(F,3,X),
+	imprimirCar(F,4,X),
+	imprimirCar(F,5,X),
+	imprimirCar(F,6,X),
+	imprimirCar(F,7,X),
+	imprimirCar(F,8,X),
+	writeln('').
+
+%Manda a imprimir el tablero
+imprimir(X):-
+	writeln('    1    2    3    4    5    6    7    8'), 
+	imprimirFila(1,X),
+	imprimirFila(2,X),
+	imprimirFila(3,X),
+	imprimirFila(4,X),
+	imprimirFila(5,X),
+	imprimirFila(6,X),
+	imprimirFila(7,X),
+	imprimirFila(8,X).
+
+
+%Verifica si el jugador uno tiene una jugada valida de movimiento en diagonal
 mov1(X1,Y1,X2,Y2):-
 	tablero(T),
 	existeFicha(X1,Y1,1),
-	X2 is (X1-1),
-	Y2 is (Y1-1),
-	X2<8,
-	X2>(-1),
-	Y2<8,
-	Y2>(-1),
+	X2 is (X1+1),
+	Y2 is (Y1+1),
+	X2<9,
+	X2>0,
+	Y2<9,
+	Y2>0,
 	not(existeFicha(X2,Y2,_)),
 	!.
 mov1(X1,Y1,X2,Y2):-
 	tablero(T),
 	existeFicha(X1,Y1,1),
-	X2 is (X1-1),
-	Y2 is (Y1+1),
-	X2<8,
-	X2>(-1),
-	Y2<8,
-	Y2>(-1),
+	X2 is (X1+1),
+	Y2 is (Y1-1),
+	X2<9,
+	X2>0,
+	Y2<9,
+	Y2>0,
 	not(existeFicha(X2,Y2,_)),
 	!.
 
