2c6a0fbbbef1e7ede29326f9ce0cffa25f67bcb5
Gustavo EL Khoury <gustavoelkhoury@gmail.com>
2014-01-15 10:47:56 -0430

    modificaciones again

--- proyecto.pl
+++ proyecto.pl
@@ -1,17 +1,17 @@
 
 %Comando que inicia el juego, asi como el tablero del mismo
-jugar:- assert(Ficha(1,2,1)),assert(Ficha(1,4,1)),
-	assert(Ficha(1,6,1)),assert(Ficha(1,8,1)),
-	assert(Ficha(2,1,1)),assert(Ficha(2,3,1)),
-	assert(Ficha(2,5,1)),assert(Ficha(2,7,1)),
-	assert(Ficha(3,2,1)),assert(Ficha(3,4,1)),
-	assert(Ficha(3,6,1)),assert(Ficha(3,8,1)),
-	assert(Ficha(6,1,2)),assert(Ficha(6,3,2)),
-	assert(Ficha(6,5,2)),assert(Ficha(6,7,2)),
-	assert(Ficha(7,2,2)),assert(Ficha(7,4,2)),
-	assert(Ficha(7,6,2)),assert(Ficha(7,8,2)),
-	assert(Ficha(8,1,2)),assert(Ficha(8,3,2)),
-	assert(Ficha(8,5,2)),assert(Ficha(8,7,2)).
+jugar:- assert(ficha(1,2,1)),assert(ficha(1,4,1)),
+	assert(ficha(1,6,1)),assert(ficha(1,8,1)),
+	assert(ficha(2,1,1)),assert(ficha(2,3,1)),
+	assert(ficha(2,5,1)),assert(ficha(2,7,1)),
+	assert(ficha(3,2,1)),assert(ficha(3,4,1)),
+	assert(ficha(3,6,1)),assert(ficha(3,8,1)),
+	assert(ficha(6,1,2)),assert(ficha(6,3,2)),
+	assert(ficha(6,5,2)),assert(ficha(6,7,2)),
+	assert(ficha(7,2,2)),assert(ficha(7,4,2)),
+	assert(ficha(7,6,2)),assert(ficha(7,8,2)),
+	assert(ficha(8,1,2)),assert(ficha(8,3,2)),
+	assert(ficha(8,5,2)),assert(ficha(8,7,2)).
 	
 impCar(X):-
 	X is 1,
@@ -29,10 +29,10 @@ impCar(X):-
 
 %verifica para imprimir un caracter del tablero
 imprimirCar(F,C):-
-	Ficha(F,C,Z),
+	ficha(F,C,Z),
 	impCar(Z),
 	!.
-imprimirCar(F,C,X):-
+imprimirCar(F,C):-
 	write('|  | ').
 
 %imprime una fila
@@ -79,38 +79,38 @@ verif(X,Y):-
 
 %Verifica si el jugador 1 tiene una jugada valida de movimiento en diagonal
 mov1(X1,Y1,X2,Y2):-
-	Ficha(X1,Y1,1),
+	ficha(X1,Y1,1),
 	X2 is (X1+1),
 	Y2 is (Y1+1),
 	verif(X2,Y2),
-	not(Ficha(X2,Y2,_)),
+	not(ficha(X2,Y2,_)),
 	!.
 mov1(X1,Y1,X2,Y2):-
-	Ficha(X1,Y1,1),
+	ficha(X1,Y1,1),
 	X2 is (X1+1),
 	Y2 is (Y1-1),
 	verif(X2,Y2),
-	not(Ficha(X2,Y2,_)),
+	not(ficha(X2,Y2,_)),
 	!.
 %Verifica para comer a izquierda
 mov1(X1,Y1,X2,Y2):-
-	Ficha(X1,Y1,1),
+	ficha(X1,Y1,1),
 	XF is X1+1,
 	YF is Y1-1,
-	(Ficha(XF,YF,2);Ficha(XF,YF,4)),
+	(ficha(XF,YF,2);ficha(XF,YF,4)),
 	verif(X2,Y2),
-	not(Ficha(X2,Y2,_)),
+	not(ficha(X2,Y2,_)),
 	X2 is X1+2,
 	Y2 is Y1-2.
 
 %Verifica para comer a derecha
 mov1(X1,Y1,X2,Y2):-
-	Ficha(X1,Y1,1),
+	ficha(X1,Y1,1),
 	XF is X1+1,
 	YF is Y1+1,
-	(Ficha(XF,YF,2);Ficha(XF,YF,4)),
+	(ficha(XF,YF,2);ficha(XF,YF,4)),
 	verif(X2,Y2),
-	not(Ficha(X2,Y2,_)),
+	not(ficha(X2,Y2,_)),
 	X2 is X1+2,
 	Y2 is Y1+2.
 
