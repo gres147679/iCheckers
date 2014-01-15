b2562b7a6ab148729330a01062d7ad8b64a38831
Gustavo EL Khoury <gustavoelkhoury@gmail.com>
2014-01-15 10:16:01 -0430

    modificaciones

--- proyecto.pl
+++ proyecto.pl
@@ -1,20 +1,18 @@
 
 %Comando que inicia el juego, asi como el tablero del mismo
-jugar :- assert(tablero(
-		 [
-		 [1,2,1],[1,4,1],[1,6,1],[1,8,1],
-		 [2,1,1],[2,3,1],[2,5,1],[2,7,1],
-		 [3,2,1],[3,4,1],[3,6,1],[3,8,1],		 
-		 [8,1,2],[8,3,2],[8,5,2],[8,7,2],
-		 [7,2,2],[7,4,2],[7,6,2],[7,8,2],
-		 [6,1,2],[6,3,2],[6,5,2],[6,7,2]
-		 ])).
+jugar:- assert(Ficha(1,2,1)),assert(Ficha(1,4,1)),
+	assert(Ficha(1,6,1)),assert(Ficha(1,8,1)),
+	assert(Ficha(2,1,1)),assert(Ficha(2,3,1)),
+	assert(Ficha(2,5,1)),assert(Ficha(2,7,1)),
+	assert(Ficha(3,2,1)),assert(Ficha(3,4,1)),
+	assert(Ficha(3,6,1)),assert(Ficha(3,8,1)),
+	assert(Ficha(6,1,2)),assert(Ficha(6,3,2)),
+	assert(Ficha(6,5,2)),assert(Ficha(6,7,2)),
+	assert(Ficha(7,2,2)),assert(Ficha(7,4,2)),
+	assert(Ficha(7,6,2)),assert(Ficha(7,8,2)),
+	assert(Ficha(8,1,2)),assert(Ficha(8,3,2)),
+	assert(Ficha(8,5,2)),assert(Ficha(8,7,2)).
 	
-% Verifica si existe una ficha en el tablero	
-existeFicha(X,Y,Z):- 
-	tablero(T), 
-	member([X,Y,Z],T).
-
 impCar(X):-
 	X is 1,
 	write('|< | ').
@@ -30,38 +28,38 @@ impCar(X):-
 
 
 %verifica para imprimir un caracter del tablero
-imprimirCar(F,C,X):-
-	member([F,C,Z],X),
+imprimirCar(F,C):-
+	Ficha(F,C,Z),
 	impCar(Z),
 	!.
 imprimirCar(F,C,X):-
 	write('|  | ').
 
 %imprime una fila
-imprimirFila(F,X):-
+imprimirFila(F):-
 	write(F),
 	write(' '),
-	imprimirCar(F,1,X),
-	imprimirCar(F,2,X),
-	imprimirCar(F,3,X),
-	imprimirCar(F,4,X),
-	imprimirCar(F,5,X),
-	imprimirCar(F,6,X),
-	imprimirCar(F,7,X),
-	imprimirCar(F,8,X),
+	imprimirCar(F,1),
+	imprimirCar(F,2),
+	imprimirCar(F,3),
+	imprimirCar(F,4),
+	imprimirCar(F,5),
+	imprimirCar(F,6),
+	imprimirCar(F,7),
+	imprimirCar(F,8),
 	writeln('').
 
 %Manda a imprimir el tablero
-imprimir(X):-
+imprimir:-
 	writeln('    1    2    3    4    5    6    7    8'), 
-	imprimirFila(1,X),
-	imprimirFila(2,X),
-	imprimirFila(3,X),
-	imprimirFila(4,X),
-	imprimirFila(5,X),
-	imprimirFila(6,X),
-	imprimirFila(7,X),
-	imprimirFila(8,X).
+	imprimirFila(1),
+	imprimirFila(2),
+	imprimirFila(3),
+	imprimirFila(4),
+	imprimirFila(5),
+	imprimirFila(6),
+	imprimirFila(7),
+	imprimirFila(8).
 
 
 
@@ -81,38 +79,38 @@ verif(X,Y):-
 
 %Verifica si el jugador 1 tiene una jugada valida de movimiento en diagonal
 mov1(X1,Y1,X2,Y2):-
-	existeFicha(X1,Y1,1),
+	Ficha(X1,Y1,1),
 	X2 is (X1+1),
 	Y2 is (Y1+1),
 	verif(X2,Y2),
-	not(existeFicha(X2,Y2,_)),
+	not(Ficha(X2,Y2,_)),
 	!.
 mov1(X1,Y1,X2,Y2):-
-	existeFicha(X1,Y1,1),
+	Ficha(X1,Y1,1),
 	X2 is (X1+1),
 	Y2 is (Y1-1),
 	verif(X2,Y2),
-	not(existeFicha(X2,Y2,_)),
+	not(Ficha(X2,Y2,_)),
 	!.
 %Verifica para comer a izquierda
 mov1(X1,Y1,X2,Y2):-
-	existeFicha(X1,Y1,1),
+	Ficha(X1,Y1,1),
 	XF is X1+1,
 	YF is Y1-1,
-	(existeFicha(XF,YF,2);existeFicha(XF,YF,4)),
+	(Ficha(XF,YF,2);Ficha(XF,YF,4)),
 	verif(X2,Y2),
-	not(existeFicha(X2,Y2,_)),
+	not(Ficha(X2,Y2,_)),
 	X2 is X1+2,
 	Y2 is Y1-2.
 
 %Verifica para comer a derecha
 mov1(X1,Y1,X2,Y2):-
-	existeFicha(X1,Y1,1),
+	Ficha(X1,Y1,1),
 	XF is X1+1,
 	YF is Y1+1,
-	(existeFicha(XF,YF,2);existeFicha(XF,YF,4)),
+	(Ficha(XF,YF,2);Ficha(XF,YF,4)),
 	verif(X2,Y2),
-	not(existeFicha(X2,Y2,_)),
+	not(Ficha(X2,Y2,_)),
 	X2 is X1+2,
 	Y2 is Y1+2.
 
